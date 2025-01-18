6d083ce0b6c953112490d70637cf6d567ac4d926
Kevin-Tucuxi <kevin@tucuxi.ai>
2025-01-17 21:10:19 -0800

    Initial Commit

---
+++ SignifcanceOfStonesTests/SignifcanceOfStonesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignifcanceOfStonesTests.swift
+//  SignifcanceOfStonesTests
+//
+//  Created by Kevin Keller on 1/17/25.
+//
+
+import Testing
+@testable import SignifcanceOfStones
+
+struct SignifcanceOfStonesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
