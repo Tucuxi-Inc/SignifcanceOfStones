7d07dedd212c6dca8f7486ab8f3f53073af2c0ca
Kevin-Tucuxi <kevin@tucuxi.ai>
2025-05-09 09:40:24 -0700

    Removing API Key from APIConfig so that next round it's deleted from Github

--- SignifcanceOfStones/Config/APIConfig.swift
+++ SignifcanceOfStones/Config/APIConfig.swift
@@ -1,5 +1,1 @@
-import Foundation
 
-enum APIConfig {
-    static let openAIKey = "sk-svcacct-tH6Fx67QNW2jzH4H0XKZciOsFY_Jb5kfoinKqsDl2nSc-fF0miRI2lyXocS2qJus_r8rrc4lnT3BlbkFJLsJWwyVstTl8yI_dvD7Pqk29F-8W6uW3aODvY8manm5W1czfxnVc9RApNVXftXfUVPe5ThaAA"
-} 
